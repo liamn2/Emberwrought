26c06929da83fe57dac1a5556221fc5ba37fea95
Liam Nutley <evildeadfan123@gmail.com>
2024-01-31 15:59:49 +0000

    Create requests.py

---
+++ requests.py
@@ -0,0 +1,16 @@
+import requests
+
+# Requests is a HTTP library in Python.
+
+>>> import requests
+>>> r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+>>> r.status_code
+200
+>>> r.headers['content-type']
+'application/json; charset=utf8'
+>>> r.encoding
+'utf-8'
+>>> r.text
+'{"authenticated": true, ...'
+>>> r.json()
+{'authenticated': True, ...}
