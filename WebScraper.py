7df4886ca14c84017ca1addcd0f61db6cf91e178
Liam Nutley <evildeadfan123@gmail.com>
2024-01-31 15:57:13 +0000

    Update WebScraper.py
    Added HTML, JavaScript and CSS parser commands.

--- WebScraper.py
+++ WebScraper.py
@@ -5,3 +5,10 @@ r = requests.get('Enter Website')      # Load webpage
 soup = bs(r.content)                   # Pull site content
 print(soup.prettify())                 # Print
 
+# Below, we will specifically be navigating a HTML document. 
+
+bs.Stylesheet()    # Returns all info in <style> tags from CSS Stylesheet in HTML document. 
+bs.Script()        # For items in <script> tags, Javascript items. 
+bs.Template()      # For HTML templates, that is, items between <template> tags.
+
+
