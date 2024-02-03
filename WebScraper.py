8c3da066b7b76d89a58b221f5fa4a47dc59b520d
Liam Nutley <evildeadfan123@gmail.com>
2024-02-03 18:38:52 +0000

    Update WebScraper.py
    Added XML parsing commands in Beautiful Soup 4.

--- WebScraper.py
+++ WebScraper.py
@@ -11,4 +11,11 @@ bs.Stylesheet()    # Returns all info in <style> tags from CSS Stylesheet in HTM
 bs.Script()        # For items in <script> tags, Javascript items. 
 bs.Template()      # For HTML templates, that is, items between <template> tags.
 
+# The following commands are for XML documents
+
+bs.Declaration()             # Represents declaration at the beginning of XML document.
+bs.Doctype()                 # Document Type Declaration at beginning of XML file. 
+bs.CData()                   # Pulls CData from XML. 
+bs.ProcessingInstruction()   # Represents contents of XML processing instruction.
+
 
