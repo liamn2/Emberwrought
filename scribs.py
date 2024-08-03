bbb5cc34233198ef19baa6d4d6f2e7d4692c60c0
Liam Nutley <evildeadfan123@gmail.com>
2024-08-03 21:35:41 +0100

    Update scribs.py
    Updated script and added note.

--- scribs.py
+++ scribs.py
@@ -1,2 +1,4 @@
-def capital_chars(string):
-  return [for i in i, where enumerate(string) ]
+def capital_chars(string):  #Define function with string input
+  return [for i in i, for char in enumerate(string), return char.index() if char.isupper()]
+  
+return capital_chars("HeLlO")
