1e1ff086aa0532eb404df3f6be2fe4abbe947b32
Liam Nutley <evildeadfan123@gmail.com>
2025-06-12 01:16:06 +0100

    Update cplusplusnoodling.cpp
    Added Collatz algorithm for C++.

--- cplusplusnoodling.cpp
+++ cplusplusnoodling.cpp
@@ -5,3 +5,26 @@ int main() {
   cout << "Hello World!";
   return 0;
 }
+
+// Collatz Algorithm
+#include <iostream>
+
+int collatz(int n) {
+    int steps = 0;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        steps++;
+    }
+    return steps;
+}
+
+int main() {
+    int start_number = 6;
+    int steps = collatz(start_number);
+    std::cout << "The Collatz sequence for " << start_number << " takes " << steps << " steps to reach 1." << std::endl;
+    return 0;
+}
