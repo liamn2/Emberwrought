9795ae78aa8ccdff511928bc689cb9527b2b224d
Liam Nutley <evildeadfan123@gmail.com>
2025-06-24 00:30:33 +0100

    Create structures.rs
    Added examples of structs in Rust.

---
+++ structures.rs
@@ -0,0 +1,14 @@
+struct Person {  //Struct to hold Perosn data.
+  name: String,
+  age: u8,
+}
+
+struct Point {  //Co-ordinates struct.
+  x: f32,
+  y: f32,
+}
+
+struct Unit; //Placeholder struct for traits with no data. 
+
+struct Pair_or_Tuple(f32, i32);
+
