17db932962cc07df4c4642286a1bffeadf195937
Liam Nutley <evildeadfan123@gmail.com>
2025-06-25 02:12:19 +0100

    Update structures.rs
    Created personal struct to test parameter setting.

--- structures.rs
+++ structures.rs
@@ -10,5 +10,11 @@ struct Point {  //Co-ordinates struct.
 
 struct Unit; //Placeholder struct for traits with no data. 
 
-struct Pair_or_Tuple(f32, i32);
+struct Pair_or_Tuple(f32, i32)
+
+struct sandwichFilling {
+  meat: String;
+  sauce: String;
+  butter: bool,
+}
 
