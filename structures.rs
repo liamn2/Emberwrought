2595c71edd2df0cb52c085c01cdf353b8f028056
Liam Nutley <evildeadfan123@gmail.com>
2025-06-26 02:08:03 +0100

    Update structures.rs
    Added notes on performing data analysis in Rust.

--- structures.rs
+++ structures.rs
@@ -18,3 +18,108 @@ struct sandwichFilling {
   butter: bool,
 }
 
+// Data Analysis
+// Scalar types
+let x = 5;     // i32
+let y = 10.5;   // f64
+
+// Compound types
+let tup = (1, "Hello", 1.5); // Tuple
+let arr = [1, 2, 3];        // Array
+
+// Collections
+let mut vec = Vec::new();   // Vector 
+vec.push(1);
+vec.push(2);
+
+let mut map = HashMap::new(); // Hashmap
+map.insert("key", 1);
+
+//Reading in CSV data.
+extern crate csv;
+
+fn main() {
+  let mut rdr = csv::Reader::from_reader(io::stdin());
+  for record in rdr.records() {
+      // Use record
+  }
+
+//Reading in JSON
+}
+
+extern crate serde;
+extern crate serde_json;
+
+use serde_json::Value;
+
+fn main() {
+  let json_str = r#"{ "name": "John Doe" }"#;
+  let v: Value = serde_json::from_str(json_str);
+
+  println!("{}", v["name"]);  
+}
+
+//SQL 
+extern crate rusqlite;
+
+use rusqlite::Connection;
+
+fn main() {
+  let conn = Connection::open("test.db").unwrap();
+
+  conn.execute(
+    "CREATE TABLE person (
+                name  TEXT, 
+                age INTEGER            
+             )", 
+    []);
+}
+
+//Plotting
+extern crate plotters;
+
+use plotters::prelude::*;
+
+fn main() {
+  let mut data = Vec::new();
+  data.push((1., 2.));
+  data.push((2., 3.));
+  data.push((3., 1.));
+
+  let mut chart = ChartBuilder::on(&data)
+                            .caption("Example plot", ("sans-serif", 50))
+                            .set_label_area_size(LabelAreaPosition::Left, 40)  
+                            .line_chart();
+
+  chart.configure_mesh()  
+     .draw()
+     .unwrap(); 
+}
+
+//Logistic regression using rustlearn.
+extern crate rustlearn;
+use rustlearn::{Dataset, LogisticRegression, Metric};
+
+fn main() {
+  let mut dataset = Dataset::new();
+  dataset.push([-1., -2.], -1);
+  dataset.push([0., 1.], 1);
+  dataset.push([2., 1.], 1);
+
+  let mut lr = LogisticRegression::default();
+  lr.fit(&dataset, Metric::Accuracy);
+
+  let pred = lr.predict(&[-1., -2.]);
+  assert_eq!(pred, -1);
+
+  let pred = lr.predict(&[1., 2.]);
+  assert_eq!(pred, 1);  
+}
+
+//Closer look at rustlearn crate for Machine Learning. 
+//Models
+//logistic regression using stochastic gradient descent,
+//support vector machines using the libsvm library,
+//decision trees using the CART algorithm,
+//random forests using CART decision trees, and
+//factorization machines.
