78aa0c57acf37c32c47c88f28d2938a742806c8a
Liam Nutley <evildeadfan123@gmail.com>
2025-07-01 06:13:40 +0100

    Create functions.rs
    Rust file for calling mathematical operation functions.

---
+++ functions.rs
@@ -0,0 +1,18 @@
+fn main() {                            //Call functions within main function.
+    sum();
+    subtraction();
+}
+
+fn sum() {
+    let number1: i32 = 7;
+    let number2: i32 = 12;
+    let result = number1 + number2;
+    println!("Your number is {}", result);
+    }
+    
+fn subtraction() {
+    let num1: i32 = 5;
+    let num2: i32 = 5;
+    let result = num1 - num2;
+    println!("The result of subtraction is {}", result);
+}
